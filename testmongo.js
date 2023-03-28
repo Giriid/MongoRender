53f1d340e30fe0e9bb52649052ed6fc61d6b11fe
Giriid <105373009+Giriid@users.noreply.github.com>
2023-03-28 12:22:16 -0500

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -35,8 +35,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ItemDb');
-    const parts = database.collection('_ItemsCollection');
+    const database = client.db('Cluster0');
+    const parts = database.collection('mydb');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
